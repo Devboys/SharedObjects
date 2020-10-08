781f56fd89bb016e7f01bd2ae2bb3a556ff5ae01
Jeppe Faber <JeppestougaardFaber@gmail.com>
2020-10-08 17:48:22 +0200

    Removed dependency on external assembly definition (woops)

---
+++ Runtime/InspectorUtilities/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,4 @@
+﻿using UnityEngine;
+
+//Intentionally empty class. Used by Unity to reference the property drawer defined in ReadOnlyDrawer.cs
+public class ReadOnlyAttribute : PropertyAttribute { }
