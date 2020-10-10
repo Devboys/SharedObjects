803d748df0b02f548ae35fe9b65d89ba6aa4b408
Jeppe Faber <JeppestougaardFaber@gmail.com>
2020-10-10 13:54:45 +0200

    Added check to prevent/detect circular game event calls (which would cause infinite loops).

--- Runtime/Events/GameEvent.cs
+++ Runtime/Events/GameEvent.cs
@@ -9,18 +9,28 @@ namespace Devboys.SharedObjects.Events
     {
         private List<GameEventListener> listeners = new List<GameEventListener>();
 
+        //boolean to detect a recursive call stack.
+        bool eventInProgress = false;
+
         /// <summary>
         /// Triggers this event to raise the "OnEventRaised()" methods for any subscribed event 
         /// listeners.
         /// </summary>
         public void Raise()
         {
+            //prevent recursive calls to this event
+            if(eventInProgress == true) throw new System.Exception("Caught recursive call to event");
+
+            eventInProgress = true;
             // loop backwards through list because listener list might change as a response to events 
             // (i.e. listeners removing themselves)
             for (int i = listeners.Count - 1; i >= 0; i--)
             {
                 listeners[i].OnEventRaised();
             }
+
+            //reset event flag when all events have been called.
+            eventInProgress = false;
         }
 
         //returns how many event listeners are currently subscribed to this event;
